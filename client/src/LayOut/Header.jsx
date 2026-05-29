8dce9261ac92521ca8112ff40cd38f07a042f221
osayl12 <hamedosayl@gmail.com>
2026-05-29 21:53:29 +0300

    העברת קובץ VARS.JS ל תקיית CLIENT כדי שכל הקומבונניטות יעבדו

--- client/src/LayOut/Header.jsx
+++ client/src/LayOut/Header.jsx
@@ -1,18 +1,18 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { PageTitle } from "../vars.jsx";
+import { PageTitle } from "../../vars.js";
 
 function Header() {
-    return (
-        <AppBar position="static" sx={{ gridArea: "header" }}>
-            <Toolbar>
-                <Typography variant="h6" component="div">
-                    {PageTitle}
-                </Typography>
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar position="static" sx={{ gridArea: "header" }}>
+      <Toolbar>
+        <Typography variant="h6" component="div">
+          {PageTitle}
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
--- client/src/_Features/Attendance/apiAttendance.js
+++ client/src/_Features/Attendance/apiAttendance.js
@@ -1,36 +1,39 @@
-import { backURL } from "../../vars.jsx";
+import { backURL } from "../../../vars.js";
 const section = "ATT";
 
 async function CheckIn(formData) {
-    const response = await fetch(`${backURL}/${section}/CheckIn`, {
-        method: "POST",
-        headers: { accept: "application/json", "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-        credentials: "include",
-    });
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    return response.json();
+  const response = await fetch(`${backURL}/${section}/CheckIn`, {
+    method: "POST",
+    headers: { accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+    credentials: "include",
+  });
+  if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+  return response.json();
 }
 
 async function CheckOut(formData) {
-    const response = await fetch(`${backURL}/${section}/CheckOut`, {
-        method: "POST",
-        headers: { accept: "application/json", "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-        credentials: "include",
-    });
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    return response.json();
+  const response = await fetch(`${backURL}/${section}/CheckOut`, {
+    method: "POST",
+    headers: { accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+    credentials: "include",
+  });
+  if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+  return response.json();
 }
 
 async function GetReport(id_number, year, month) {
-    const response = await fetch(`${backURL}/${section}/Report/${id_number}/${year}/${month}`, {
-        method: "GET",
-        headers: { accept: "application/json" },
-        credentials: "include",
-    });
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-    return response.json();
+  const response = await fetch(
+    `${backURL}/${section}/Report/${id_number}/${year}/${month}`,
+    {
+      method: "GET",
+      headers: { accept: "application/json" },
+      credentials: "include",
+    },
+  );
+  if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+  return response.json();
 }
 
 export { CheckIn, CheckOut, GetReport };
