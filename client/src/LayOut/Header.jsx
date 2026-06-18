400710191f6cc6b2abeda4e39834d54b03a28246
osayl12 <hamedosayl@gmail.com>
2026-06-18 12:40:27 +0300

    הוספת כפתור המבורגר למובייל עם זיהוי גדול מסך

--- client/src/LayOut/Header.jsx
+++ client/src/LayOut/Header.jsx
@@ -1,18 +1,42 @@
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import { PageTitle } from "../../vars.js";
+import { AppBar, Toolbar, Typography, IconButton, useMediaQuery, useTheme, Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { HeaderHeight } from '../theme_params.jsx';
+import { PageTitle } from '../../vars.js';
 
-function Header() {
-  return (
-    <AppBar position="static" sx={{ gridArea: "header" }}>
-      <Toolbar>
-        <Typography variant="h6" component="div">
-          {PageTitle}
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
+function Header({ onMenuClick }) {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+    return (
+        <AppBar
+            position="static"
+            sx={{
+                height: `${HeaderHeight}px`,
+                '& .MuiToolbar-root': {
+                    height: `${HeaderHeight}px`,
+                    minHeight: `${HeaderHeight}px`,
+                },
+            }}
+        >
+            <Toolbar>
+                {isMobile && (
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={onMenuClick}
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                )}
+                <Typography variant="h6" component="h1" sx={{ pr: 3 }}>
+                    {PageTitle}
+                </Typography>
+                <Box sx={{ flexGrow: 1 }} />
+            </Toolbar>
+        </AppBar>
+    );
 }
 
 export default Header;
