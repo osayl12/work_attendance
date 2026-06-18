11214a55114b0ba21c558bdd7e9d1ebaa9f273b6
osayl12 <hamedosayl@gmail.com>
2026-06-18 12:39:49 +0300

    החלפת BOX ב PAPER

--- client/src/LayOut/Footer.jsx
+++ client/src/LayOut/Footer.jsx
@@ -1,24 +1,28 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { FooterHeight } from "../theme_params.jsx";
-import { PageTitle } from "../../vars.js";
+import { Paper, Typography } from '@mui/material';
+import { FooterHeight, FooterTxtColor } from '../theme_params.jsx';
+import { PageTitle } from '../../vars.js';
 
 function Footer() {
-  return (
-    <Box
-      sx={{
-        gridArea: "footer",
-        height: FooterHeight,
-        backgroundColor: "primary.main",
-        color: "white",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Typography variant="body2">© 2026 {PageTitle}</Typography>
-    </Box>
-  );
+    return (
+        <Paper
+            component="footer"
+            sx={{
+                gridArea: 'footer',
+                height: FooterHeight,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                paddingX: 2,
+                backgroundColor: 'primary.main',
+                color: FooterTxtColor,
+                borderRadius: 0,
+            }}
+        >
+            <Typography variant="body2">
+                &copy; {new Date().getFullYear()} {PageTitle}
+            </Typography>
+        </Paper>
+    );
 }
 
 export default Footer;
